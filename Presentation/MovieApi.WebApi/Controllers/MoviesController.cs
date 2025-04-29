7c0d5529253a18d4c5edd3deef9949ba8f559b1e
Guney <guneycin@gmail.com>
2025-04-29 09:38:37 +0300

    🚀 Implemented full CRUD endpoints with CQRS pattern and DI setup
    Command/Query Enhancements:
    
    Added constructors to command/query classes:
    
    RemoveCategoryCommand
    
    RemoveMovieCommand
    
    GetCategoryByIdQuery
    
    GetMovieByIdQuery
    
    Enabled proper parameter passing for all operations
    
    Dependency Injection Setup:
    
    Registered all handlers in Program.cs
    Temporary verbose registration (to be refactored later)
    
    Controller Implementation:
    
    Added complete MoviesController with:
    
    GET (list & by ID)
    
    POST (create)
    
    PUT (update)
    
    DELETE
    
    Added matching CategoriesController

---
+++ Presentation/MovieApi.WebApi/Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly GetMovieQueryHandler _getMovieQueryHandler;
+        private readonly GetMovieByIdQueryHandler _getMovieByIdQueryHandler;
+        private readonly CreateMovieCommandHandler _createMovieCommandHandler;
+        private readonly UpdateMovieCommandHandler _updateMovieCommandHandler;
+        private readonly RemoveMovieCommandHandler _removeMovieCommandHandler;
+
+        public MoviesController(GetMovieQueryHandler getMovieQueryHandler, GetMovieByIdQueryHandler getMovieByIdQueryHandler, CreateMovieCommandHandler createMovieCommandHandler, UpdateMovieCommandHandler updateMovieCommandHandler, RemoveMovieCommandHandler removeMovieCommandHandler)
+        {
+            _getMovieQueryHandler = getMovieQueryHandler;
+            _getMovieByIdQueryHandler = getMovieByIdQueryHandler;
+            _createMovieCommandHandler = createMovieCommandHandler;
+            _updateMovieCommandHandler = updateMovieCommandHandler;
+            _removeMovieCommandHandler = removeMovieCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MovieList()
+        {
+            var value = await _getMovieQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie(CreateMovieCommand command)
+        {
+            await _createMovieCommandHandler.Handle(command);
+            return Ok("Film ekleme işlemi başarılı.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMovie(UpdateMovieCommand command)
+        {
+            await _updateMovieCommandHandler.Handle(command);
+            return Ok("Film güncelleme işlemi başarılı.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            await _removeMovieCommandHandler.Handle(new RemoveMovieCommand(id));
+            return Ok("Film silme işlemi başarılı.");
+        }
+
+        [HttpGet("GetMovie")]
+        public async Task<IActionResult> GetMovie(int id)
+        {
+            var value = await _getMovieByIdQueryHandler.Handle(new GetMovieByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
