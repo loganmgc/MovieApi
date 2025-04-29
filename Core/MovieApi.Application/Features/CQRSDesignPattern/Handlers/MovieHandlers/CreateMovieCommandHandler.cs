8e2ba98bc031fa08ec96ede5ccff083c454956d3
Guney <guneycin@gmail.com>
2025-04-29 08:19:49 +0300

    🚀 Added CQRS implementation for Movie and Category operations in new Application layer
    Created new Application layer project (MovieApi.Application.csproj)
    
    Implemented CQRS pattern structure:
    
    Commands: Added CRUD operations for both Movie and Category entities
    
    Create/Update/Remove commands
    
    Queries: Implemented GetById functionality
    
    Handlers: Added corresponding command handlers
    
    Results: Defined query result DTOs
    
    Organized with clear separation:
    
    Distinct folders for Commands, Queries, Handlers, and Results
    
    Consistent naming between Movie and Category operations

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class CreateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+        public CreateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async void Handler(CreateMovieCommand command)
+        {
+            _context.Movies.Add(new Movie
+            {
+                Title = command.Title,
+                CoverImageUrl = command.CoverImageUrl,
+                Rating = command.Rating,
+                Description = command.Description,
+                Duration = command.Duration,
+                ReleaseDate = command.ReleaseDate,
+                CreatedYear = command.CreatedYear,
+                Status = command.Status
+            });
+            await _context.SaveChangesAsync();
+        }   
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async void Handler(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.Title = command.Title;
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.Rating = command.Rating;
+            value.Description = command.Description;
+            value.Duration = command.Duration;
+            value.ReleaseDate = command.ReleaseDate;
+            value.CreatedYear = command.CreatedYear;
+            value.Status = command.Status;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
