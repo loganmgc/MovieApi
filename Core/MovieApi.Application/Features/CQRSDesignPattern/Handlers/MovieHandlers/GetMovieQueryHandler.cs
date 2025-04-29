d926aadaf3e2603a87b2ffb9b9c822ed336a1a6f
Guney <guneycin@gmail.com>
2025-04-29 08:42:05 +0300

    ✨ Added query handlers for Movie and Category listings

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResults;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieQueryHandler
+    {
+        private readonly MovieContext _context;
+        public GetMovieQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<GetMovieQueryResult>> Handle()
+        {
+            var values = await _context.Movies.ToListAsync();
+            return values.Select(x => new GetMovieQueryResult
+            {
+                MovieId = x.MovieId,
+                Title = x.Title,
+                CoverImageUrl = x.CoverImageUrl,
+                Rating = x.Rating,
+                Description = x.Description,
+                Duration = x.Duration,
+                ReleaseDate = x.ReleaseDate,
+                CreatedYear = x.CreatedYear,
+                Status = x.Status,
+            }).ToList();
+        }
+    }
+}
